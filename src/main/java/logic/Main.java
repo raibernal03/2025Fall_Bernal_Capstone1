286155f952698b0bba485898f8ffa3419841c215
Naarai Bernal <110580725+raibernal03@users.noreply.github.com>
2025-10-16 13:42:35 -0500

    adding the while loop and try and catches

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             menu(scanner);
@@ -12,7 +15,7 @@ public class Main {
     }
 
     //main menu
-    public static void menu(Scanner scanner){
+    public static void menu(Scanner scanner) {
         FileManager.headerLogo("src/main/resources/headers/accounting-ledger-app-ascii.txt");
 
         System.out.println("D) Deposit");
@@ -20,120 +23,137 @@ public class Main {
         System.out.println("L) Ledger");
         System.out.println("X) Exit");
         System.out.print("--> ");
-
-        char choice = scanner.nextLine().toUpperCase().charAt(0);
-        switch (choice) {
-            case 'D':
-                Ledger.addTransaction(scanner, "positive");
-                break;
-            case 'P':
-                Ledger.addTransaction(scanner, "negative");
-                break;
-            case 'L':
-                ledgerMenu(scanner);
-                break;
-            case 'X':
-                scanner.close();
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Invalid Choice, Please try again\n");
+        try {
+            char choice = scanner.nextLine().toUpperCase().charAt(0);
+            switch (choice) {
+                case 'D':
+                    Ledger.addTransaction(scanner, "positive");
+                    break;
+                case 'P':
+                    Ledger.addTransaction(scanner, "negative");
+                    break;
+                case 'L':
+                    ledgerMenu(scanner);
+                    break;
+                case 'X':
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Choice, Please try again\n");
+            }
+            System.out.println("\n");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
-        System.out.println("\n");
 
     }
 
     //goes to ledger menu
     public static void ledgerMenu(Scanner scanner) {
-        FileManager.headerLogo("src/main/resources/headers/ledger-menu-ascii.txt");
-        System.out.println("\n\nA) All - Displays all entries");
-        System.out.println("D) Show all Deposits");
-        System.out.println("P) Show all Payments");
-        System.out.println("R) Reports");
-        System.out.println("H) Home");
-        System.out.print("--> ");
+        boolean running = true;
+        while (running) {
+            FileManager.headerLogo("src/main/resources/headers/ledger-menu-ascii.txt");
+            System.out.println("\n\nA) All - Displays all entries");
+            System.out.println("D) Show all Deposits");
+            System.out.println("P) Show all Payments");
+            System.out.println("R) Reports");
+            System.out.println("H) Home");
+            System.out.print("--> ");
+            try {
+                char choice = scanner.nextLine().toUpperCase().charAt(0);
+                switch (choice) {
+                    case 'A':
+                        FileManager.headerLogo("src/main/resources/headers/all-transactions-ascii.txt");
+                        List<Transaction> transactions = FileManager.readFile();
+                        printList(transactions);
 
-        char choice = scanner.nextLine().toUpperCase().charAt(0);
-        switch (choice) {
-            case 'A':
-                FileManager.headerLogo("src/main/resources/headers/all-transactions-ascii.txt");
-                List<Transaction> transactions = FileManager.readFile();
-                printList(transactions);
-                ledgerMenu(scanner);
-                break;
-            case 'D':
-                //Show all positive transactions
-                FileManager.headerLogo("src/main/resources/headers/deposits-ascii.txt");
-                Ledger.depositPayment("deposits");
+                        break;
+                    case 'D':
+                        //Show all positive transactions
+                        FileManager.headerLogo("src/main/resources/headers/deposits-ascii.txt");
+                        Ledger.depositPayment("deposits");
 
-                ledgerMenu(scanner);
-                break;
-            case 'P':
-                FileManager.headerLogo("src/main/resources/headers/payments-ascii.txt");
-                Ledger.depositPayment("payments");
-                ledgerMenu(scanner);
-                break;
-            case 'R':
-                reportsMenu(scanner);
-                break;
-            case 'H':
-                menu(scanner);
-                break;
-            default:
-                System.out.println("Invalid Choice, Please try again\n\n");
-                ledgerMenu(scanner);
+                        break;
+                    case 'P':
+                        FileManager.headerLogo("src/main/resources/headers/payments-ascii.txt");
+                        Ledger.depositPayment("payments");
+                        break;
+                    case 'R':
+                        reportsMenu(scanner);
+                        break;
+                    case 'H':
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid Choice, Please try again\n\n");
+
+                        break;
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
     //Reports menu
     public static void reportsMenu(Scanner scanner) {
-        FileManager.headerLogo("src/main/resources/headers/reports-menu-ascii.txt");
-        System.out.println("1) Month to Date");
-        System.out.println("2) Previous Month");
-        System.out.println("3) Year to Date");
-        System.out.println("4) Previous Year");
-        System.out.println("5) Search by Vendor");
-        System.out.println("0) Back");
-        System.out.print("--> ");
+        boolean running = true;
+        while (running) {
 
-        int choice = Integer.parseInt(scanner.nextLine());
+            FileManager.headerLogo("src/main/resources/headers/reports-menu-ascii.txt");
+            System.out.println("1) Month to Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year to Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search by Vendor");
+            System.out.println("0) Back");
+            System.out.print("--> ");
+            try {
+                int choice = Integer.parseInt(scanner.nextLine());
 
-        switch (choice) {
-            case 1:
-                //Sends to month to date
-                FileManager.headerLogo("src/main/resources/headers/current-month-report-ascii.txt");
-                Reports.mtd();
-                reportsMenu(scanner);
-                break;
-            case 2:
-                //send to previous month
-                Reports.previousMonth();
-                reportsMenu(scanner);
-                break;
-            case 3:
-                //send to year to date
-                Reports.ytd();
-                reportsMenu(scanner);
-                break;
-            case 4:
-                //send to previous year
-                Reports.previousYear();
-                reportsMenu(scanner);
-                break;
-            case 5:
-                //send to search by vendor
-                System.out.print("Enter vendor name: ");
-                String vendor = scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        //Sends to month to date
+                        FileManager.headerLogo("src/main/resources/headers/current-month-report-ascii.txt");
+                        Reports.mtd();
+
+                        break;
+                    case 2:
+                        //send to previous month
+                        Reports.previousMonth();
+
+                        break;
+                    case 3:
+                        //send to year to date
+                        Reports.ytd();
+
+                        break;
+                    case 4:
+                        //send to previous year
+                        Reports.previousYear();
+
+                        break;
+                    case 5:
+                        //send to search by vendor
+                        System.out.print("Enter vendor name: ");
+                        String vendor = scanner.nextLine();
+                        System.out.println("\n");
+                        Reports.vendorSearch(vendor);
+
+                        break;
+                    case 0:
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid Choice, Please try again\n");
+
+                        break;
+                }
                 System.out.println("\n");
-                Reports.vendorSearch(vendor);
-                reportsMenu(scanner);
-                break;
-            case 0:
-                ledgerMenu(scanner);
-            default:
-                System.out.println("Invalid Choice, Please try again\n");
-                reportsMenu(scanner);
-                break;
+            } catch(Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
