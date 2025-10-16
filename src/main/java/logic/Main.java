6c59d703a42297be2cbfe5f83379261c1e88378b
Naarai Bernal <110580725+raibernal03@users.noreply.github.com>
2025-10-16 11:16:33 -0500

    changing looks

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            FileManager.headerLogo("src/main/resources/headers/accounting-ledger-app-ascii.txt");
             menu(scanner);
         }
     }
 
     //main menu
     public static void menu(Scanner scanner){
+        FileManager.headerLogo("src/main/resources/headers/accounting-ledger-app-ascii.txt");
 
         System.out.println("D) Deposit");
         System.out.println("P) Make Payment");
